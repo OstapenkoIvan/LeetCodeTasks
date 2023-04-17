0fa335e4858c5042dca497cadd3fadb60c6bae52
OstapenkoIvan <ostapenko.ivan@gmail.com>
2023-04-17 20:38:58 +0300

    6th task

--- scripts.js
+++ scripts.js
@@ -160,3 +160,11 @@ while (nums.indexOf(val) >= 0) {
   nums.splice(nums.indexOf(val), 1);
 }
 return nums.length;
+
+// 6. Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
